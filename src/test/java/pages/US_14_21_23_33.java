a2cdda379c4814c9a4563b2ed0a2c3e66e1432e8
Sema <baharsu2002@gmail.com>
2023-03-09 20:00:22 +0000

    Kendi branchimi olusturdum

---
+++ src/test/java/pages/US_14_21_23_33.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class US_14_21_23_33 {
+
+    public US_14_21_23_33() {
+
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+}
