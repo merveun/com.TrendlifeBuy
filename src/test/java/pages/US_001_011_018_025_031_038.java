309713d789e05d100a4fb83cb447469dfa3bc3e3
Abdurrahman Alas <63901054+AbdurrahmanAlas@users.noreply.github.com>
2023-03-09 23:04:55 +0300

    Pages classı tamamlandı

---
+++ src/test/java/pages/US_001_011_018_025_031_038.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class US_001_011_018_025_031_038 {
+
+    public US_001_011_018_025_031_038() {
+
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+}
