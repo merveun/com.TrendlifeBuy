c61826f4ae2f06c2982deedd2f9015efbf0e707c
merve <merveunal9444@gmail.com>
2023-03-09 22:34:37 +0300

    first commit

---
+++ src/test/java/pages/US_2_35.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class US_2_35 {
+
+    public US_2_35() {
+
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+}
---
+++ src/test/java/stepdefinitions/US_2_35_StepDef.java
@@ -0,0 +1,4 @@
+package stepdefinitions;
+
+public class US_2_35_StepDef {
+}
