52519e58003a8c77217a9a0cb2331f2c6d9d8c05
Berker OĞUZ <tberkeroguz@gmail.com>
2023-03-09 23:04:35 +0300

    calismami tamamladim

--- src/test/java/pages/US_2_35.java
+++ src/test/java/pages/US_2_35.java
@@ -4,9 +4,10 @@ import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
 public class US_2_35 {
-
     public US_2_35() {
 
         PageFactory.initElements(Driver.getDriver(),this);
     }
+
+    // Yorumumu ekledim.
 }
