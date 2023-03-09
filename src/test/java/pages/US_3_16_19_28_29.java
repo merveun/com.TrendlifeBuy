4c934eef475eabc8119a2929fa806beed55a12b9
mustafazdl <117026915+mustafazdl@users.noreply.github.com>
2023-03-09 23:10:11 +0300

    first code

---
+++ src/test/java/pages/US_3_16_19_28_29.java
@@ -0,0 +1,13 @@
+package pages;
+
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class US_3_16_19_28_29 {
+
+        public US_3_16_19_28_29() {
+
+            PageFactory.initElements(Driver.getDriver(),this);
+        }
+    }
+
