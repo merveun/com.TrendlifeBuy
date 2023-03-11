d99f378094ece8c7e24a9838d2e572d3f2a6949f
mustafazdl <117026915+mustafazdl@users.noreply.github.com>
2023-03-12 01:09:12 +0300

    1203

--- src/test/java/pages/US_3_16_19_28_29.java
+++ src/test/java/pages/US_3_16_19_28_29.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
@@ -9,5 +11,20 @@ public class US_3_16_19_28_29 {
 
             PageFactory.initElements(Driver.getDriver(),this);
         }
+
+    // US_003 ait locate'ler
+
+    //HomePage>Login
+    @FindBy(xpath = "(//a[text()='Login'])[1]")
+    public WebElement HomePageLogin;
+
+
+
+
+
+
+
+
+
     }
 
