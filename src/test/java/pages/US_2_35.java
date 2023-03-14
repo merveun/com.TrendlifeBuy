a9fd33a95b78da441459ad7df6f777afc9b1c7e9
merve <merveunal9444@gmail.com>
2023-03-14 22:39:10 +0300

    US_2 locateler

--- src/test/java/pages/US_2_35.java
+++ src/test/java/pages/US_2_35.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
@@ -8,5 +10,68 @@ public class US_2_35 {
 
         PageFactory.initElements(Driver.getDriver(),this);
     }
+
+    //Homepage/Header > playstore button
+    @FindBy(xpath = "//span[text()='Play store']")
+    public WebElement playstoreButton;
+
+    //Homepage/Header > appstore button
+    @FindBy(xpath = "//span[text()='Play store']")
+    public WebElement appstoreButton;
+
+    //Homepage/Header > track your order button
+    @FindBy(xpath = "//span[text()='Track Your Order']")
+    private WebElement trackyourorderButton;
+
+    //Homepage/Header > compare button
+    @FindBy(xpath = "//span[text()='Compare(']")
+    private WebElement compareButton;
+
+    //Homepage/Header > wishlist button
+    @FindBy(xpath = "//span[text()='Wishlist (']")
+    private WebElement wishlistButton;
+
+
+    //Homepage/Header > cart button
+    @FindBy(xpath = "//span[text()='Cart (']")
+    private WebElement cartButton;
+
+    //Homepage/Header > logo
+    @FindBy(xpath = "(//img[@src=\"https://qa.trendlifebuy.com/public/uploads/settings/63985b425a3e6.png\"])[1]")
+    private WebElement logo;
+
+    //Homepage/Header/Menu > home button
+    @FindBy(xpath = "(//a[@href=\"https://qa.trendlifebuy.com/\"])[1]")
+    private WebElement homeButton;
+
+    //Homepage/Header/Menu > blog button
+    @FindBy(xpath = "(//a[@href=\"https://qa.trendlifebuy.com/\"])[1]")
+    private WebElement blogButton;
+
+    //Homepage/Header/Menu > aboutus button
+    @FindBy(xpath = "(//a[@href=\"https://qa.trendlifebuy.com/\"])[1]")
+    private WebElement aboutusButton;
+
+    //Homepage/Header/Menu > contact button
+    @FindBy(xpath = "(//a[@href=\"https://qa.trendlifebuy.com/\"])[1]")
+    private WebElement contactButton;
+
+    //Homepage/Header_right > login button
+    @FindBy(xpath = "(//a[@href=\"https://qa.trendlifebuy.com/\"])[1]")
+    private WebElement loginButton;
+
+    //Homepage/Header_right > register button
+    @FindBy(xpath = "(//a[@href=\"https://qa.trendlifebuy.com/\"])[1]")
+    private WebElement registerButton;
+
+    //Homepage/Header_right > new user zone button
+    @FindBy(xpath = "(//a[@href=\"https://qa.trendlifebuy.com/\"])[1]")
+    private WebElement newuserzoneButton;
+
+    //Homepage/Header > searchbox
+    @FindBy(xpath = "(//input[@class=\"form-control category_box_input\"]")
+    private WebElement searchBox;
+
+
 
 }
--- src/test/java/stepdefinitions/US_2_35_StepDef.java
+++ src/test/java/stepdefinitions/US_2_35_StepDef.java
@@ -1,4 +1,8 @@
 package stepdefinitions;
 
+import pages.US_2_35;
+
 public class US_2_35_StepDef {
+
+    US_2_35 pages;
 }
